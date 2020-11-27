65846bfb0e426105b87f6f6137d5112901e369ef
PeterBurgs <peterburgs.vn@gmail.com>
2020-11-27 19:54:11 +0700

    finish signup screen UI

--- App.js
+++ App.js
@@ -1,21 +1,81 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  createAppContainer,
+  createSwitchNavigator,
+} from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from "react-native";
 
-export default function App() {
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from "react-native/Libraries/NewAppScreen";
+
+// Import Screens
+import SignUpScreen from "./src/screens/SignUpScreen";
+
+// Import Context
+
+// Navigator
+// const switchNavigator = createSwitchNavigator({});
+
+// App
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SignUpScreen />
+    </>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: "absolute",
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: "600",
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: "400",
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: "700",
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: "600",
+    padding: 4,
+    paddingRight: 12,
+    textAlign: "right",
   },
 });
+
+export default App;
---
+++ src/components/NavLink.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Text, TouchableOpacity, StyleSheet } from "react-native";
+import Spacer from "./Spacer";
+import { withNavigation } from "react-navigation";
+
+const NavLink = ({ navigation, text, routeName }) => {
+  return (
+    <TouchableOpacity onPress={() => navigation.navigate(routeName)}>
+      <Spacer>
+        <Text style={styles.link}>{text}</Text>
+      </Spacer>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  link: {
+    fontSize: 13,
+    alignSelf: "center",
+    color: "#d35d6e",
+  },
+});
+
+export default NavLink;
