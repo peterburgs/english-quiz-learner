61b9f03f3036e526958a85947feaeb39fa148e33
PeterBurgs <peterburgs.vn@gmail.com>
2020-12-02 10:38:29 +0700

    Beautify Tab Bar

---
+++ src/common/navigationTheme.js
@@ -0,0 +1,10 @@
+import { DefaultTheme } from "@react-navigation/native";
+
+export default {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: "#fd3a69",
+    background: "#eeeeee",
+  },
+};
